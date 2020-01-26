57e1f5ca4f09d4e493576d774cc32e71a833c42b
MolkovD <molkovd@yandex.ru>
2020-01-26 20:11:56 +0300

    added vue components 'skills' and 'reviews'

---
+++ src/scripts/reviews.js
@@ -0,0 +1,67 @@
+import Vue from "vue";
+import Flickity from "vue-flickity";
+
+new Vue({
+	el: "#reviews",
+  components: {
+    Flickity
+  },
+
+  data() {
+    return {
+      flickityOptions: {
+        initialIndex: 0,
+        prevNextButtons: false,
+        pageDots: false,
+        wrapAround: false,
+        groupCells: true
+      },
+      reviews: []
+    };
+  },
+
+  methods: {
+    next() {
+			this.$refs.flickity.next();
+			this.checkArrows();
+    },
+
+    previous() {
+			this.$refs.flickity.previous();
+			this.checkArrows();
+    },
+
+		makeRequiredImages(data) {
+			return data.map(item => {
+				const requireImage = require(`../images/content/${item.avatar}`);
+				item.avatar = requireImage;
+				console.log(item)
+				return item;
+			})
+		},
+
+		checkArrows() {
+			let prev = this.$el.querySelector(".reviews__arrow--prev");
+			let next = this.$el.querySelector(".reviews__arrow--next");
+
+			if (this.$refs.flickity.selectedIndex() === 0) {
+				prev.disabled = true;
+				return;
+			};
+
+			if (this.$refs.flickity.selectedIndex() === this.$refs.flickity.slides().length - 1) {
+				next.disabled = true;
+				return;
+			}; 
+
+			prev.disabled = false;
+			next.disabled = false;
+		}
+  },
+
+  created() {
+    const data = require("../json/reviews.json");
+		this.reviews = this.makeRequiredImages(data);
+		console.log(this.reviews)
+  }
+});
--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,49 @@
-console.log('this is skills module');
+import Vue from "vue";
+
+const skill = {
+	template: '#skill',
+	props: {
+		skillName: String,
+		skillPercent: String
+	},
+	methods: {
+		drawCirclePercent() {
+			const circle = this.$refs["color-circle"];
+			const levelPercent = 376.8 / 100 * (+this.skillPercent);
+			const dasharray = `${levelPercent} ${376.8 - levelPercent}`;
+
+			circle.style.strokeDasharray = dasharray;
+		}
+	},
+	mounted() {
+		this.drawCirclePercent();
+	}
+}
+
+const skillsList = {
+	template: '#skills-list',
+	props: {
+		skillList: Object
+	},
+	components: {
+		skill
+	}
+}
+
+new Vue({
+	el: '#skills-component',
+	template: '#skills-container',
+	data() {
+		return {
+			skills: []
+		}
+	},
+	components: {
+		skillsList
+	},
+	created() {
+		this.skills = require('../json/skills.json');
+		console.log(this.skills[0].name);
+	}
+});
+
